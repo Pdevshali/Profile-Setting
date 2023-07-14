059668a78c7933b4ad938489e6de0067a73218b2
Pdevshali <prashantdevshali0099@gmail.com>
2023-07-14 21:32:56 +0530

    changes in user profile

--- app/src/main/java/com/example/profilesetting/MainActivityCreateProfile.kt
+++ app/src/main/java/com/example/profilesetting/MainActivityCreateProfile.kt
@@ -1,20 +1,31 @@
 package com.example.profilesetting
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
+import java.net.URI
 
 class MainActivityCreateProfile : AppCompatActivity() {
+
+    private lateinit var selectedImg : URI
+    private lateinit var ProfileImage : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_createprofile)
 
         val profileButton = findViewById<TextView>(R.id.Profile)
+        ProfileImage = findViewById(R.id.profilepic)
         val editButton = findViewById<ImageButton>(R.id.edit)
         val vertButton = findViewById<ImageButton>(R.id.vert)
         val editProfileButton = findViewById<ImageButton>(R.id.imageButton1)
@@ -24,15 +35,20 @@ class MainActivityCreateProfile : AppCompatActivity() {
         val friendsButton = findViewById<ImageButton>(R.id.imagebutton5)
         val logoutButton = findViewById<ImageButton>(R.id.imageButton6)
 
+        ProfileImage.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.type = "image/*"
+            startActivityForResult(intent, 1)
+        }
+
         profileButton.setOnClickListener {
             showToast("Profile button clicked")
         }
 
         editButton.setOnClickListener {
             showToast("Edit button clicked")
-            val intent = Intent(this, MainActivityEdit::class.java)
-            startActivity(intent)
-            finish()
+
         }
 
         vertButton.setOnClickListener {
@@ -81,6 +97,8 @@ class MainActivityCreateProfile : AppCompatActivity() {
 
         friendsButton.setOnClickListener {
             showToast("Invite Friends button clicked")
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
 
         logoutButton.setOnClickListener {
@@ -92,5 +110,23 @@ class MainActivityCreateProfile : AppCompatActivity() {
 
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+
+    // to set the profile of user with selected image from gallery
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            val imageUri: Uri? = data?.data
+            imageUri?.let {
+                val bitmap = if (Build.VERSION.SDK_INT < 28) {
+                    MediaStore.Images.Media.getBitmap(contentResolver, it)
+                } else {
+                    val source = ImageDecoder.createSource(contentResolver, it)
+                    ImageDecoder.decodeBitmap(source)
+                }
+                ProfileImage.setImageBitmap(bitmap)
+            }
+        }
     }
 }
