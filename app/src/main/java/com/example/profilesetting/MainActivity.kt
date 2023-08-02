e78574b8ac323db0616728e0e29213e11f80e3af
Pdevshali <prashantdevshali0099@gmail.com>
2023-08-02 21:38:14 +0530

    Created registration and signIn Activities

--- app/src/main/java/com/example/profilesetting/MainActivity.kt
+++ app/src/main/java/com/example/profilesetting/MainActivity.kt
@@ -4,17 +4,52 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etFullName: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var etPhoneNumber: EditText
+    private lateinit var etAddress: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnRgister: Button
+    lateinit var db : FirebaseFirestore
+
+    // Define a variable to store the latitude and longitude values
+    private var latitude: Double = 0.0
+    private var longitude: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val Btn = findViewById<Button>(R.id.btn)
-        Btn.setOnClickListener {
-            val intent = Intent(this, MainActivityCreateProfile::class.java)
-            startActivity(intent)
-            finish()
+        etFullName = findViewById(R.id.etFullName)
+        etEmail = findViewById(R.id.etEmail)
+        etPhoneNumber = findViewById(R.id.etPhoneNumber)
+        etAddress = findViewById(R.id.etAddress)
+        etPassword = findViewById(R.id.etPassword)
+        btnRgister = findViewById(R.id.btnRegister)
+
+        db = FirebaseFirestore.getInstance()
+
+
+
+        btnRgister.setOnClickListener {
+
+// getting the text
+            val fName = etFullName.text.toString().trim()
+            val email = etEmail.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+            val phone = etPhoneNumber.text.toString().trim()
+            val locationAddress = etAddress.text.toString().trim()
         }
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/profilesetting/Sign_In_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.profilesetting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sign_In_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
