cf4a85ea53d16288606f3e5a2e37d0f9596a31a6
Pdevshali <prashantdevshali0099@gmail.com>
2023-08-08 21:49:04 +0530

    Created match Activity

---
+++ app/src/main/java/com/example/profilesetting/ShowMatch_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.profilesetting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowMatch_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_match)
+    }
+}
